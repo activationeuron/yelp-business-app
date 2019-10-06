5d3c60f96ea9fdac0ae1b91138c7728e97833248
amitrai <aforamitrai@gmail.com>
2019-10-06 13:18:23 +0530

    allmost done...

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses/',
   headers: {
-    Authorization:
-      'Bearer dCEkmXqEo5Q_T3_yrZeKgvR7_heJFu42i2NIXJxvm9Wj8jvX89KhYGShn1tDlQKwhTqe5m9aNnYZm5pRioHPW3hcZDDEyrUDHMxmLjMUAJa9cg8qGwJpt3WePZqMXXYx',
+    Authorization: 'Bearer ADD_SECRATE_KEY_YELP',
   },
 });
